8cd0f3a4e350a3033f8fba53fff02116a5debe52
Nikoliadis <nnikoliadis@nyc.gr>
2026-01-03 09:13:55 +0200

    First Commit

---
+++ lostfound/src/test/java/com/nikoliadis/swe6002/lostfound/LostfoundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikoliadis.swe6002.lostfound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostfoundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
