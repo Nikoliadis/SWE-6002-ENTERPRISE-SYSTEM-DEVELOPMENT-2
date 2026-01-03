ce01a9028e64c60d60eb8afffcb6857d4c8671c6
Nikoliadis <nnikoliadis@nyc.gr>
2026-01-03 10:54:38 +0200

    Added Admin Roles

---
+++ lostfound/src/main/java/com/nikoliadis/swe6002/lostfound/config/DataInitializer.java
@@ -0,0 +1,34 @@
+package com.nikoliadis.swe6002.lostfound.config;
+
+import com.nikoliadis.swe6002.lostfound.model.User;
+import com.nikoliadis.swe6002.lostfound.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class DataInitializer {
+
+    @Bean
+    CommandLineRunner initAdmin(UserRepository userRepository,
+                                PasswordEncoder passwordEncoder) {
+
+        return args -> {
+            if (userRepository.findByUsername("admin").isEmpty()) {
+
+                User admin = new User();
+                admin.setUsername("admin");
+                admin.setEmail("admin@lostfound.com");
+                admin.setPassword(passwordEncoder.encode("admin123"));
+                admin.setRole("ROLE_ADMIN");
+
+                userRepository.save(admin);
+
+                System.out.println("✅ Admin user created");
+            } else {
+                System.out.println("ℹ️ Admin already exists");
+            }
+        };
+    }
+}
